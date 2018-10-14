b03961f01a2b7a348c6ce2926b05347ff966e7a2
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-10-14 18:17:59 +0200

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,9 +1,26 @@
 const Discord = require('discord.js');
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
   if (message.member.hasPermission('KICK_MEMBERS') || message.member.hasPermission('ADMINISTRATOR') || message.author.id == 302837596600664065) {
   let user = message.mentions.users.first();
   let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted');
-  if (!muteRole) return message.reply('<:TBstop:500713271532453890> Je ne trouve pas de role "Muted".');
+  if(!muteRole){
+    try{
+      muterole = await message.guild.createRole({
+        name: "Muted",
+        color: "#000001",
+        permissions:[]
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(muterole, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false,
+          VIEW_CHANNEL: false
+        });
+      });
+    }catch(e){
+      console.log(e.stack);
+    }
+  }
   
 
  if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.replay("<:TBstop:500713271532453890> Je n'ai pas les bonnes permissions").catch(console.error);
