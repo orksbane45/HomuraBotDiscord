7ab3436be72d08b7f3918efd6d6eba6c0ba98fc3
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-10-14 18:25:19 +0200

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -3,24 +3,7 @@ exports.run = async (client, message, args) => {
   if (message.member.hasPermission('KICK_MEMBERS') || message.member.hasPermission('ADMINISTRATOR') || message.author.id == 302837596600664065) {
   let user = message.mentions.users.first();
   let muteRole = client.guilds.get(message.guild.id).roles.get('name', 'Muted');
-  if(!muteRole){
-    try{
-      muterole = await message.guild.createRole({
-        name: "Muted",
-        color: "#000001",
-        permissions:[]
-      })
-      message.guild.channels.forEach(async (channel, id) => {
-        await channel.overwritePermissions(muterole, {
-          SEND_MESSAGES: false,
-          ADD_REACTIONS: false,
-          VIEW_CHANNEL: false
-        });
-      });
-    }catch(e){
-      console.log(e.stack);
-    }
-  };
+  if (!muteRole) return message.channel.send("Aucun rôle \"Muted\" n'a était trouver sur le serveur merci d'en crée un pour pouvoir mute.")
   
 
  if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.replay("<:TBstop:500713271532453890> Je n'ai pas les bonnes permissions").catch(console.error);
