36bac2f141451fbb98db9657095b6cd9f241307d
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2018-11-23 21:00:57 +0100

    Add files via upload

---
+++ commands/killme.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+  
+    message.channel.send(`${message.author.username} est mort.`).then(Message => {
+        setTimeout(() => { Message.edit("Réanimation..."); }, 1000);
+        setTimeout(() => { Message.edit(`Réanimation réussite re bienvenue, ${message.author.username}`); }, 1000);
+    });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'killme',
+  description: 'fun',
+  usage: 'killme',
+  aliase: ['Aucun aliase n\'est disponible pour cette commandes.']
+};
