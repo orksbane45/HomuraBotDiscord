2eaf8e128a57320319a20444ac6a8a5852bd4ee5
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2019-01-01 13:25:00 +0100

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -17,7 +17,7 @@ exports.run = function(client, message, args) {
     .addField('Serveurs ',`${client.guilds.size}`, true)
     .addField('Channels',`${client.channels.size}`, true)
     .addField('Uptime', uptime , true)
-    .addField('Créateur ',`<@318619541942960128>`, true)
+    .addField('Créateur ',`<@302837596600664065>`, true)
     .addField('Partenaires','`t!partenariat`')
     .setTimestamp()
     message.channel.send(msg)
