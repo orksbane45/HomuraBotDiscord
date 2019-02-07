77061271cdf141058491b5d900116f7eaf4c7929
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2019-02-07 18:45:10 +0100

    Update partenariat.js

--- commands/partenariat.js
+++ commands/partenariat.js
@@ -3,7 +3,7 @@ exports.run = function(client, message, args) {
 
   const msg = new Discord.RichEmbed()
 
-    .setColor("RANDOM")
+    .setColor("#3C2A66")
     .setTitle("Liste des Discord/BOT partenaire :")
     .setDescription("https://discord.gg/Ycw3sqq")
     .setImage('https://cdn.discordapp.com/attachments/525783553682702377/535166066356125716/AnimeWorld.png')
