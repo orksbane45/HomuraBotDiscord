61570e59caabc35333cb64ca6f7133f3c830c985
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2019-02-09 15:18:57 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,8 +3,8 @@ const Discord = require('discord.js');
 exports.run = (client, message) => {
   
   var InviteEmbed = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .addField("<:TBtsukibot:500715036583395329> Lien d'invitation du bot !",'https://discordapp.com/oauth2/authorize?client_id=500699327262359574&scope=bot&permissions=-1')
+    .setColor("1E201C")
+    .addField("<:HBdiscord:543117211640922143> Lien d'invitation du bot !",'https://discordapp.com/oauth2/authorize?client_id=500699327262359574&scope=bot&permissions=-1')
 
   message.channel.send(InviteEmbed)
 };
@@ -18,7 +18,7 @@ exports.conf = {
 
 exports.help = {
   name: 'invite',
-  description: 'Invitez le bot sur votre discord !',
+  description: 'Invite le bot sur votre discord !',
   usage: 'invite',
   aliase: ['Aucun aliase n\'est disponible pour cette commandes.']
 };
