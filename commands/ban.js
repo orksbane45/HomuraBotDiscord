21f6909bc04937baaee13e3bfacba7514c4b64f5
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2019-02-09 14:45:14 +0100

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,18 +4,18 @@ exports.run = (client, message, args) => {
    if (message.member.hasPermission('BAN_MEMBERS') || message.member.hasPermission('ADMINISTRATOR') || message.author.id == 302837596600664065) {
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
-  if (message.mentions.users.size < 1) return message.reply("<:TBstop:500713271532453890> Vous devez mentionner quelqu'un pour le kick.").catch(console.error);
+  if (message.mentions.users.size < 1) return message.reply("<:HBinfo:543117222097453059> Vous devez mentionner quelqu'un pour le ban.").catch(console.error);
  
   
 
-  if (!message.guild.member(user).bannable) return message.reply("<:TBstop:500713271532453890> Je ne peut pas ban cet utilisateur.");
+  if (!message.guild.member(user).bannable) return message.reply("<:HBstop:543117234273386516>  Je ne peut pas ban cet utilisateur.");
   message.guild.ban(user, 2);
 
   
 
-      message.channel.send(`${user.tag} ban !`)
+      message.channel.send(`<:HByes:543783599883681825> ${user.tag} ban !`)
    }else{
-     message.reply("<:TBstop:500713271532453890> Vous n'avez pas les permissions requises pour ban un utilisateur");
+     message.reply("<:HBconfig:543117201817862145> Vous n'avez pas les permissions requises pour ban un utilisateur");
    };
 };
 
@@ -29,6 +29,6 @@ exports.conf = {
 exports.help = {
   name: 'ban',
   description: 'Ban un utilisateur.',
-  usage: 'ban [mention]',
-  aliase: ['Aucun aliase n\'est disponible pour cette commandes.']
+  usage: 'ban <mention>',
+  aliase: ['Aucun aliase n\'est disponible pour cette commande.']
 };
