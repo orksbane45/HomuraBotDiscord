b7a2ae70e4b559ac8342b47e4df0f2e2773f88e6
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2019-02-09 15:38:34 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,13 +3,10 @@ exports.run = (client, message) => {
     .then(msg => {
 
 
-        if (msg.createdTimestamp - message.createdTimestamp < 200)
-      msg.edit('<:TBgoodping:500711838799888384> : ' + (msg.createdTimestamp - message.createdTimestamp) + 'ms');
+        
+      msg.edit('<:HBinfo:543117222097453059>' + (msg.createdTimestamp - message.createdTimestamp) + ' ms');
 
-  if (msg.createdTimestamp - message.createdTimestamp >= 200)
-      msg.edit('<:TBbadping:500711761012326421> : ' + (msg.createdTimestamp - message.createdTimestamp) + 'ms');
-
-
+ 
     });
 };
 
@@ -24,5 +21,5 @@ exports.help = {
   name: 'ping',
   description: 'Affiche le ping du bot !',
   usage: 'ping',
-  aliase: ['Aucun aliase n\'est disponible pour cette commandes.']
+  aliase: ['Aucun aliase n\'est disponible pour cette commande.']
 };
