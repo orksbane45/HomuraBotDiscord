ed5ede2681bb42cf5bffc90fab5746bc17d66e1c
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2019-02-09 15:10:40 +0100

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -19,7 +19,7 @@ exports.run = (client, message, args) => {
     exports.conf = {
       enabled: true,
       guildOnly: false,
-      aliases: [],
+      aliases: ["cl"],
       permLevel: 0
     };
 
