6417348122b720d0829d69bfcf494c5ea6b610ab
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2019-02-09 15:00:08 +0100

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -1,174 +1,195 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
   let user = message.mentions.users.first();
-  let nb = Math.floor((Math.random() * 24) + 1);
-  if (message.mentions.users.size < 1) return message.reply("vous devez mentionner quelqu'un pour le hug.").catch(console.error);
+  let nb = Math.floor((Math.random() * 27) + 1);
+  if (message.mentions.users.size < 1) return message.reply("<:HBconfig:543117201817862145> Vous devez mentionner quelqu'un pour lui fait un câlin.").catch(console.error);
     if (nb == 1) {
    msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442055795723730944/hug1.gif')
     message.channel.send(msg)
   }
   if (nb == 2) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442055876355031070/hug7.gif')
     message.channel.send(msg)
   }
   if (nb == 3) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442055926455861269/hug9.gif')
     message.channel.send(msg)
   }
   if (nb == 4) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442056046941306881/hug8.gif')
     message.channel.send(msg)
   }
   if (nb == 5) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442056056995315713/hug5.gif')
     message.channel.send(msg)
   }
   if (nb == 6) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442056059537063936/hug3.gif')
     message.channel.send(msg)
   }
   if (nb == 7) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442056085705064468/hug11.gif')
     message.channel.send(msg)
   }
   if (nb == 8) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442056087202431021/hug6.gif')
     message.channel.send(msg)
   }
   if (nb == 9) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442056087953211412/hug12.gif')
     message.channel.send(msg)
   }
   if (nb == 10) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442717849643909121/hug10.gif')
     message.channel.send(msg)
   }
   if (nb == 11) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442718011783118849/hug15.gif')
     message.channel.send(msg)
   }
   if (nb == 12) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442718105094062080/hug14.gif')
     message.channel.send(msg)
   }
   if (nb == 13) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442718357469397023/hug4.gif')
     message.channel.send(msg)
   }
   if (nb == 14) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442718531914563585/hug13.gif')
     message.channel.send(msg)
   }
   if (nb == 15) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('')
     message.channel.send(msg)
   }
   if (nb == 16) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442719490833121281/hug16.gif')
     message.channel.send(msg)
   }
   if (nb == 17) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.discordapp.com/attachments/442055343544074240/442719523624321024/hug17.gif')
     message.channel.send(msg)
   }
   if (nb == 18) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.weeb.sh/images/rkx1dJ25z.gif')
     message.channel.send(msg)
   }
   if (nb == 19) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.weeb.sh/images/ByPGRkFVz.gif')
     message.channel.send(msg)
   }
   if (nb == 20) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.weeb.sh/images/S18oOuQw-.gif')
     message.channel.send(msg)
   }
   if (nb == 21) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.weeb.sh/images/HkQs_dXPZ.gif')
     message.channel.send(msg)
   }
   if (nb == 22) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.weeb.sh/images/B1wRd_XP-.gif')
     message.channel.send(msg)
   }
   if (nb == 23) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.weeb.sh/images/r1R3_d7v-.gif')
     message.channel.send(msg)
   }
   if (nb == 24) {
   msg = new Discord.RichEmbed()
-    .setTitle(`<a:TBGhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
-    .setColor('RANDOM')
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
     .setImage('https://cdn.weeb.sh/images/HJU2OdmwW.gif')
+    message.channel.send(msg)
+  }
+  if (nb == 25) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
+    .setImage('https://media.discordapp.net/attachments/489448852882063360/543793072031662093/tenor_1.gif')
+    message.channel.send(msg)
+  }
+  if (nb == 26) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
+    .setImage('https://media.discordapp.net/attachments/489448852882063360/543793071867822083/tenor.gif')
+    message.channel.send(msg)
+  }
+  if (nb == 27) {
+  msg = new Discord.RichEmbed()
+    .setTitle(`<a:HBgifhug:527589131581128706> ${user.username}, vous avez reçu un câlin de ${message.author.username}`)
+    .setColor('3C2A66')
+    .setImage('https://media.discordapp.net/attachments/489448852882063360/543793059045965834/TR2G.gif')
     message.channel.send(msg)
   }
 
@@ -183,5 +204,6 @@ exports.run = (client, message, args) => {
   exports.help = {
     name: 'hug',
     description: 'Callin !',
-    usage: 'hug [mention]'
+    usage: 'hug [mention]',
+    aliase: ['Aucun aliase n\'est disponible pour cette commande.']
   }
