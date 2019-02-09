0110048115367000d7bdc3d22ac4181b719b4d82
orksbane45 <39961514+orksbane45@users.noreply.github.com>
2019-02-09 15:32:18 +0100

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -3,18 +3,22 @@ exports.run = async (client, message, args) => {
   if (message.member.hasPermission('KICK_MEMBERS') || message.member.hasPermission('ADMINISTRATOR') || message.author.id == 302837596600664065) {
   let user = message.mentions.users.first();
   let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'Muted');
-  if (!muteRole) return message.channel.send("Aucun rôle \"Muted\" n'a était trouver sur le serveur merci d'en crée un pour pouvoir mute.")
+  if (!muteRole) return message.channel.send("<:HBconfig:543117201817862145> Aucun rôle \"Muted\" n'a était trouver sur le serveur merci d'en crée un pour pouvoir mute.")
   
 
- if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.replay("<:TBstop:500713271532453890> Je n'ai pas les bonnes permissions").catch(console.error);
+ if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.replay("<:HBstop:543117234273386516> Je n'ai pas les bonnes permissions").catch(console.error);
 
 
+if (message.guild.member(user).hasRole(muteRole))  message.guild.member(user).removeRole(muteRole).then(()=> {
 
+   
+            message.channel.sendMessage(`<:HByes:543783599883681825> **${user.username}** est maintenant unmute !`)
+    });
 
  message.guild.member(user).addRole(muteRole).then(()=> {
 
    
-            message.channel.sendMessage(`${user.username} est maintenant mute !`)
+            message.channel.sendMessage(`<:HByes:543783599883681825> **${user.username}** est maintenant mute !`)
     });
   }
 };
