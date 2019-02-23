1cdfdd414bd99386985db4036ce278f8c0b3a532
Tsuki <39961514+orksbane45@users.noreply.github.com>
2019-02-23 14:44:10 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,9 +11,9 @@ exports.run = (client, message, params) => {
   .setColor('#3C2A66')
   .setAuthor('Liste des commandes :')
   .addField('<:HBconfig:543117201817862145> Administration/Modération',' `purge`, `mute`, `blacklist`, `kick`, `ban`, `avote`, `asay`')
-  .addField('<:HBgame:543120810689167361> Fun',' `roll`, `master`, `8ball`, `say`, `vote`, `killme`, `hug`, `kiss`')
+  .addField('<:HBgame:543120810689167361> Fun',' `roll`, `master`, `8ball`, `say`, `vote[REWORK]`, `killme`, `hug[REWORK]`, `kiss`')
   .addField('<:HBinfo:543117222097453059> Information',' `stats`, `serveurinfo`, `invite`, `ping`, `help`, `profile`, `avatar`, `changelog`')
-  .setDescription("<:HBconfig:543117201817862145> Mise a jour du bot... <:HBconfig:543117201817862145>\rUtilise la commande `h!help <commande>` pour plus d'informations.")
+  .setDescription("Utilise la commande `h!help <commande>` pour plus d'informations.\rTu peut aussi voir toutes les informations sur les mise a jour du bot avec la commande `h!changelog`.")
   .setTimestamp()
 
               message.channel.send(msg);
