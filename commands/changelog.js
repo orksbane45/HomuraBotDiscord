95e5e6096ea6c62d75800086fad8a778ea511f01
Tsuki <39961514+orksbane45@users.noreply.github.com>
2019-02-23 14:41:03 +0100

    Update changelog.js

--- commands/changelog.js
+++ commands/changelog.js
@@ -7,11 +7,10 @@ exports.run = (client, message, args) => {
   
     var embed = new Discord.RichEmbed()
     .setColor("3C2A66")
-    .setTitle("Derniere mise a jour : 09/02/2019")
-    .addField("Modification des emotes du bot","<:HBconfig:543117201817862145> <:HBsupport:543117250593554462> <:HBstop:543117234273386516> <:HBdiscord:543117211640922143> <:HBgame:543120810689167361> <:HByes:543783599883681825> <:HBno:543784274650726403> <:HBinfo:543117222097453059>")
-    .addField("Nouvelle commande (ou en devellopement)","`h!changelog`, `h!kiss`")
-    .addField("Modification du client","Nom : Homura Bot\rprefix : h!")
-    .addField("Rajout de 3 gif pour la commande `h!hug`","https://media.discordapp.net/attachments/489448852882063360/543793059045965834/TR2G.gif\rhttps://media.discordapp.net/attachments/489448852882063360/543793071867822083/tenor.gif\rhttps://media.discordapp.net/attachments/489448852882063360/543793072031662093/tenor_1.gif")
+    .setTitle("Derniere mise a jour : 23/02/2019")
+    .addField("Jeux","Il y a maintenant 3 easters eggs sur le bot a vous de vous amusez a les trouver !")
+    .addField("Commandes qui arriverons prochainement", "`h!image` , `h!gif` , `h!character`")
+    
     message.channel.send(embed)
 
     
