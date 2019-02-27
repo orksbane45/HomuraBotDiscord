ae922429167686fe20d2465a60c99e9d22ccbb01
Tsuki <39961514+orksbane45@users.noreply.github.com>
2019-02-27 17:04:20 +0100

    Create character.js

---
+++ commands/character.js
@@ -0,0 +1,42 @@
+const ch = require('../character.json');
+const Discord = require('discord.js');
+const fs = require('fs');
+
+exports.run = (client, message, params) => {
+  message.delete()
+  const embed2 = new Discord.RichEmbed()
+  .setColor('#3C2A66')
+  .setTitle('Utilisation : `h!character <personnage>`')
+  .setDescription("Personnages : Madoka , Homura , Sakura , Mami , Nagisa , Sayaka , Kyuubey , Charlotte")
+  
+
+  if (!params[0] || !ch[params[0]]) return message.channel.send(embed2);
+  let character = ch[params[0]];
+
+  msg = new Discord.RichEmbed()
+        .setColor('#3C2A66')
+        .setTitle('Informations sur : ' + params[0])
+        .addField('Nom : ', character.nom , true)
+        .addField('Age : ', character.age , true)
+        .addField('Description : ', character.description , true)
+        .addField('Couleur de cheveux : ', character.cheveux ,true)
+        .addField('Arme :', character.arme ,true)
+        .setThumbnail(character.image)
+
+
+        message.channel.send(msg);
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'roleinfo',
+  description: 'Vous donne toutes les informations sur les personnages de Madoka Magica.',
+  usage: 'character <personnage>',
+  aliase: ['Aucun aliase n\'est disponible pour cette commande.']
+};
