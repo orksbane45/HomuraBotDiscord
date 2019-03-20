f3750af21fd1dbdc8f4fbc5fb692bdd86e22ed11
Tsuki <39961514+orksbane45@users.noreply.github.com>
2019-03-20 15:44:41 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,11 +10,11 @@ exports.run = (client, message, params) => {
 
   .setColor('#3C2A66')
   .setAuthor('Liste des commandes :')
-  .addField('<:HBconfig:543117201817862145> Administration/Modération',' `purge`, `mute`, `blacklist`, `kick`, `ban`, `avote`, `asay`')
-  .addField('<:HBgame:543120810689167361> Fun',' `roll`, `master`, `8ball`, `say`, `vote[REWORK]`, `killme`, `hug[REWORK]`, `kiss`')
+  .addField('<:HBconfig:543117201817862145> Administration/Modération',' `purge`, `mute`, `blacklist`, `kick`, `ban`, `avote`')
+  .addField('<:HBgame:543120810689167361> Fun',' `roll`, `master`, `8ball`, `say`, `killme`, `hug[REWORK]`, `kiss`')
   .addField('<:HBhomura:550341880198463489> Anime (madoka)',' `character`, `kyuubey`')
-  .addField('<:HBinfo:543117222097453059> Information',' `stats`, `serveurinfo`, `invite`, `ping`, `help`, `profile`, `avatar`, `changelog`')
-  .setDescription("Utilise la commande `h!help <commande>` pour plus d'informations.\rTu peut aussi voir toutes les informations sur les mise a jour du bot avec la commande `h!changelog`.")
+  .addField('<:HBinfo:543117222097453059> Information',' `suggestion`, `vote`, `stats`, `serveurinfo`, `invite`, `ping`, `help`, `profile`, `avatar`, `changelog`')
+  .setDescription("Utilise la commande `h!help <commande>` pour plus d'informations.\rTu peux aussi voir toutes les informations sur les mises a jour du bot avec la commande `h!changelog`.")
   .setTimestamp()
 
               message.channel.send(msg);
