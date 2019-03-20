cc055282f56a23f40559a5e2cafe34f732246c5b
Tsuki <39961514+orksbane45@users.noreply.github.com>
2019-03-20 15:48:37 +0100

    Update partenariat.js

--- commands/partenariat.js
+++ commands/partenariat.js
@@ -4,9 +4,11 @@ exports.run = function(client, message, args) {
   const msg = new Discord.RichEmbed()
 
     .setColor("#3C2A66")
-    .setTitle("Liste des Discord/BOT partenaire :")
-    .setDescription("https://discord.gg/Ycw3sqq")
-    .setImage('https://cdn.discordapp.com/attachments/525783553682702377/535166066356125716/AnimeWorld.png')
+    .setTitle("Liste des Discords/BOTs partenaire :")
+    .addField("Anime World" , "https://discord.gg/Ycw3sqq")
+    .addField("?" , "https://discord.gg/GUa6sZj")
+    .addField("• Andaria •" , "https://discord.gg/mSnF9K4")
+    
     
     
 
