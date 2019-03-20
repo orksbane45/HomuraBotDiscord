f4f3aa99d22029493d888146170093f37c2f83fe
Tsuki <39961514+orksbane45@users.noreply.github.com>
2019-03-20 15:08:40 +0100

    Update killme.js

--- commands/killme.js
+++ commands/killme.js
@@ -1,9 +1,27 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
-  
+   let nb = Math.floor((Math.random() * 3) + 1);
+  if (nb == 1) {
+   
     message.channel.send(`${message.author.username} est mort.`).then(Message => {
-        setTimeout(() => { Message.edit("Réanimation..."); }, 1000);
-        setTimeout(() => { Message.edit(`Réanimation réussite re bienvenue, ${message.author.username}`); }, 1000);
+        setTimeout(() => { Message.edit("Réanimation..."); }, 3000);
+        setTimeout(() => { Message.edit(`Réanimation réussite re bienvenue, ${message.author.username}`); }, 3000);
+    }
+                                                                      
+   if (nb == 2) {
+   
+    message.channel.send(`${message.author.username} est mort.`).then(Message => {
+        setTimeout(() => { Message.edit("Réanimation..."); }, 3000);
+        setTimeout(() => { Message.edit(`${message.author.username}, Le médecin s'approche de votre lit et vous annonce "Le seul moyen que j'ai trouvé pour vous sauvez était de vous donner les organes de Lyze qui est malheureusement mort sur le coup."`); }, 3000);
+    }
+                                                                      
+    if (nb == 3) {
+   
+    message.channel.send(`${message.author.username} est mort.`).then(Message => {
+        setTimeout(() => { Message.edit("Réanimation..."); }, 3000);
+        setTimeout(() => { Message.edit(`Malheureusement, ${message.author.username} la réanimation n'a pas sauver notre cher ami.`); }, 3000);
+    }                                                                  
+                                                                      
     });
 };
 
