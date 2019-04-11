ca315d78128cc9c82aa2aa81c31f0265a04d1b0d
Tsuki <39961514+orksbane45@users.noreply.github.com>
2019-04-11 15:09:29 +0200

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -1,36 +1,87 @@
 exports.run = function(client, message, args) {
   let nb = Math.floor((Math.random() * 10) + 1);
-      if (nb == 1) {
-        message.channel.sendMessage('1');
-      }
-      if (nb == 2) {
-        message.channel.sendMessage('2');
-      }
-      if (nb == 3) {
-        message.channel.sendMessage('3');
-      }
-      if (nb == 4) {
-        message.channel.sendMessage('4');
-      }
-      if (nb == 5) {
-        message.channel.sendMessage('5');
-      }
-      if (nb == 6) {
-        message.channel.sendMessage('6');
-      }
-      if (nb == 7) {
-        message.channel.sendMessage('7');
-      }
-      if (nb == 8) {
-        message.channel.sendMessage('8');
-      }
-      if (nb == 9) {
-        message.channel.sendMessage('9');
-      }
-      if (nb == 10) {
-        message.channel.sendMessage('10');
-      }
-
+  
+  if (nb == 1) {
+   
+    message.channel.send(`Chargement.`).then(Message => {
+        setTimeout(() => { Message.edit("Chargement.."); }, 1000);
+        setTimeout(() => { Message.edit("Chargement..."); }, 1000);
+        setTimeout(() => { Message.edit(`:one:`); }, 2000);
+   });
+    }
+  if (nb == 2) {
+   
+    message.channel.send(`Chargement.`).then(Message => {
+        setTimeout(() => { Message.edit("Chargement.."); }, 1000);
+        setTimeout(() => { Message.edit("Chargement..."); }, 1000);
+        setTimeout(() => { Message.edit(`:two:`); }, 2000);
+   });
+    }
+  if (nb == 3) {
+   
+    message.channel.send(`Chargement.`).then(Message => {
+        setTimeout(() => { Message.edit("Chargement.."); }, 1000);
+        setTimeout(() => { Message.edit("Chargement..."); }, 1000);
+        setTimeout(() => { Message.edit(`:three:`); }, 2000);
+   });
+    }
+  if (nb == 4) {
+   
+    message.channel.send(`Chargement.`).then(Message => {
+        setTimeout(() => { Message.edit("Chargement.."); }, 1000);
+        setTimeout(() => { Message.edit("Chargement..."); }, 1000);
+        setTimeout(() => { Message.edit(`:four:`); }, 2000);
+   });
+    }
+  if (nb == 5) {
+   
+    message.channel.send(`Chargement.`).then(Message => {
+        setTimeout(() => { Message.edit("Chargement.."); }, 1000);
+        setTimeout(() => { Message.edit("Chargement..."); }, 1000);
+        setTimeout(() => { Message.edit(`:five:`); }, 2000);
+   });
+    }
+  if (nb == 6) {
+   
+    message.channel.send(`Chargement.`).then(Message => {
+        setTimeout(() => { Message.edit("Chargement.."); }, 1000);
+        setTimeout(() => { Message.edit("Chargement..."); }, 1000);
+        setTimeout(() => { Message.edit(`:six:`); }, 2000);
+   });
+    }
+  if (nb == 7) {
+   
+    message.channel.send(`Chargement.`).then(Message => {
+        setTimeout(() => { Message.edit("Chargement.."); }, 1000);
+        setTimeout(() => { Message.edit("Chargement..."); }, 1000);
+        setTimeout(() => { Message.edit(`:seven:`); }, 2000);
+   });
+    }
+  if (nb == 8) {
+   
+    message.channel.send(`Chargement.`).then(Message => {
+        setTimeout(() => { Message.edit("Chargement.."); }, 1000);
+        setTimeout(() => { Message.edit("Chargement..."); }, 1000);
+        setTimeout(() => { Message.edit(`:eight:`); }, 2000);
+   });
+    }
+  if (nb == 9) {
+   
+    message.channel.send(`Chargement.`).then(Message => {
+        setTimeout(() => { Message.edit("Chargement.."); }, 1000);
+        setTimeout(() => { Message.edit("Chargement..."); }, 1000);
+        setTimeout(() => { Message.edit(`:nine:`); }, 2000);
+   });
+    }
+  if (nb == 10) {
+   
+    message.channel.send(`Chargement.`).then(Message => {
+        setTimeout(() => { Message.edit("Chargement.."); }, 1000);
+        setTimeout(() => { Message.edit("Chargement..."); }, 1000);
+        setTimeout(() => { Message.edit(`:keycap_ten:`); }, 2000);
+   });
+    }
+  
 }
 
       exports.conf = {
