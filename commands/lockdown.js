5e8b9f38f0978a8b561067c4d2759a71acfe3b78
Tsuki <39961514+orksbane45@users.noreply.github.com>
2019-05-15 16:15:21 +0200

    Create lockdown.js

---
+++ commands/lockdown.js
@@ -0,0 +1,49 @@
+const ms = require('ms');
+exports.run = (client, message, args) => {
+  if (!client.lockit) client.lockit = [];
+  const time = args.join(' ');
+  const validUnlocks = ['release', 'unlock', 'off', 'stop'];
+  if (!time) return message.reply('<:HBconfig:543117201817862145> Veuillez joindre une durée à ce blocage, en heures, en minutes ou en secondes.');
+
+  if (validUnlocks.includes(time)) {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: null
+    }).then(() => {
+      message.channel.send('Blocage suspendu.');
+      clearTimeout(client.lockit[message.channel.id]);
+      delete client.lockit[message.channel.id];
+    }).catch(error => {
+      console.log(error);
+    });
+  } else {
+    message.channel.overwritePermissions(message.guild.id, {
+      SEND_MESSAGES: false
+    }).then(() => {
+      message.channel.send(`<:HByes:543783599883681825> Salon bloqué pour ${ms(ms(time), { long:true })}`).then(() => {
+
+        client.lockit[message.channel.id] = setTimeout(() => {
+          message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: null
+          }).then(message.channel.send('<:HByes:543783599883681825> Blocage suspendu.')).catch(console.error);
+          delete client.lockit[message.channel.id];
+        }, ms(time));
+
+      }).catch(error => {
+        console.log(error);
+      });
+    });
+  }
+};
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['ld'],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'lockdown',
+  description: 'Cette commande bloque un salon écrit pour une durée donnée.',
+  usage: 'lockdown <durée>',
+  aliase: ['ld']
+};
