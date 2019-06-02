43b516483a90ba09d70a027af16d6d164ec78ba6
Tsuki <39961514+orksbane45@users.noreply.github.com>
2019-06-02 17:56:13 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ const moment = require('moment');
 module.exports = client => {
  console.log(chalk.black.bgRed('Bot en ligne'));
  console.log(chalk.inverse(`[${moment().format('DD-MM-YYYY HH:mm:ss')}] `) + chalk.black.bgGreen(`${client.user.tag} est prêt.`));
-  client.user.setActivity('h!help',{type:'LISTENING'})
+  client.user.setActivity('h!help | TsukiH',{type:'LISTENING'})
 };
 // on off
